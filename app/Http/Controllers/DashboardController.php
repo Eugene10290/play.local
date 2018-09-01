96b40ccd8f16c25cb9d83647bf8e9673057adb9b
Eugene Tkachenko <zhenytkachenko70@gmail.com>
2018-09-01 15:33:54 +0300

    Роли для главной страницы админки

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,14 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('permission:role-list,role-create,role-edit,role-delete,
+                         user-create,user-list,user-update,user-delete,
+                         blog-create,blog-edit,blog-delete');
+    }
+
     public function index(){
         return view('dashboard');
     }
