f5f8af05b5008ca3292d1d2c58e3516e6ff84237
Eugene Tkachenko <zhenytkachenko70@gmail.com>
2018-09-04 21:38:19 +0300

    добавлен app.blade удалённый

---
+++ resources/views/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>head</title>
+    <link href="{{ asset('public/css/head/style.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('public/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" >
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <header style="background-image: url(img/header_fon.jpg);">
+            <div class="head-div">
+                <a href="#"><h2>Vitaliy Stulnev</h2></a>
+                <ul>
+                    <li><a href="#" class="link">Главная</a></li>
+                    <li><a href="#" class="link">Играй с душой</a></li>
+                    <li><a href="#" class="link">Блог</a></li>
+                    <li><a href="#" class="link">Ноты</a></li>
+                    <li><a href="#" class="link">Поддержка проекта</a></li>
+                    <li><a href="#" class="link">О нас</a></li>
+                </ul>
+            </div>
+            <div class="left-view-site">
+                <h1>Моя задача научить вас играть</h1>
+            </div>
+            <div class="right-view-site">
+                <h3>Крутой заголовок</h3>
+                <p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Прямо бросил диких вопрос они, рукописи своего. Текст большого коварных возвращайся себя безопасную буквенных, имеет рыбными назад ты лучше реторический.</p>
+            </div>
+        </header>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="content">
+
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <footer>
+            <a href="#"><img src="{{ asset('public/img/youtube.png') }}" alt=""></a>
+            <a href="#"><img src="{{ asset('public/img/facebook.png') }}" alt=""></a>
+            <a href="#"><img src="{{ asset('public/img/Instagram.png') }}" alt=""></a>
+            <a href="#"><img src="{{ asset('public/img/g+.png') }}" alt=""></a>
+            <p>&#169; 2018 Stulnev Vitaliy. All rights reserved.</p>
+        </footer>
+    </div>
+</div>
+
+</body>
+</html>
