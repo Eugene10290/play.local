ebb71e5ad27735716c4ab9eca0f0ab522fb59eed
Oleg <stulnev.oleg@gmail.com>
2018-11-02 20:02:22 +0300

    создание главной страницы блога

--- resources/views/blog/user/index.blade.php
+++ resources/views/blog/user/index.blade.php
@@ -1,34 +1,37 @@
 @extends('layouts\app')
 @section('content')
     @foreach($blog as $n)
-        <a href="{{ url('admin/blog', $n->slug) }}"><img class="news-image"  src="{{ asset('images/uploads/blogImages/'.$n->wall) }}" alt="{{ $n->title }}"></a>
-        <div class="news-description">
-            <h3><a href="{{ url('admin/blog', $n->slug) }}">{{ $n->title }}</a></h3>
-            <time datetime="{{$n->published_at}}"><span class="fa fa-clock-o fa-1x"></span>{{$n->getBeautifulDateAttribute()}}</time>
-            @permission('blog-edit')
-                <a class="admin btn btn-primary btn-xs" href="{{url('admin/blog/'.$n->id.'/edit')}}">Редактировать</a>
-            @endpermission
-            @unless($n->tags->isEmpty())
-                <h5>Тэги</h5>
-                <ul>
+        <div class="img-blog-article"  alt="{{ $n->title }}")">
+            <a href="{{ url('/blog', $n->slug) }}">
+                <div class="bgc-img"></div>
+                <img class="news-image"  src="{{ asset('images/uploads/blogImages/'.$n->wall) }}" alt="{{ $n->title }}">
+             </a>
+            <div class="news-description">
+                <h3 class="article-title"><a href="{{ url('admin/blog', $n->slug) }}">{{ $n->title }}</a></h3>
+                <time class="time-article" datetime="{{$n->published_at}}"><span class="fa fa-clock-o fa-1x"></span>{{$n->getBeautifulDateAttribute()}}</time>
+                @permission('blog-edit')
+                    <a class="button-edit admin btn btn-primary btn-xs" href="{{url('admin/blog/'.$n->id.'/edit')}}">Редактировать</a>
+                @endpermission
+                @unless($n->tags->isEmpty())
+                <ul class="tag-common">
                     @foreach($n->tags as $tag)
-                        <li><a href="{{ url('tags/'.$tag->name) }}">{{$tag->name}} </a></li>
+                    <li class="tag-blog"><a href="{{ url('tags/'.$tag->name) }}">{{$tag->name}} </a></li>
                     @endforeach
                 </ul>
-            @endunless
-            @permission('blog-delete')
-                {!! Form::open(['url' => 'admin/blog/'.$n->id, 'action' => 'BlogController@destroy', 'method' => 'DELETE', 'class' => 'admin button'])!!}
-                {!! Form::token() !!}
-                {!! Form::submit('Удалить',
-                     [
-                        'class' => 'btn btn-danger btn-xs confirm',
+                @endunless
+                @permission('blog-delete')
+                    {!! Form::open(['url' => 'admin/blog/'.$n->id, 'action' => 'BlogController@destroy', 'method' => 'DELETE', 'class' => 'admin button'])!!}
+                    {!! Form::token() !!}
+                    {!! Form::submit('Удалить',
+                    [
+                        'class' => 'delete-button btn btn-danger btn-xs confirm',
                         'onclick' => 'return confirm("Удалить статью?")'
-                     ])
-                 !!}
-                {!! Form::close() !!}
-            @endpermission()
-
-        </div>
+                    ])
+                    !!}
+                    {!! Form::close() !!}
+                @endpermission()
+            </div>
+</div>
     @endforeach
     <script src="{{ asset('public/js/jquery-3.3.1.min.js') }}"></script>
 
